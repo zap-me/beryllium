5d64cc8e7947aab7fbbdfb1bd49e86505047a234
Alex <aboyle@zap.me>
2022-07-19 10:15:18 +1200

    Add an extra pace to inline comments to resolve error E261 in pycodestyle linter

--- src/tasks.py
+++ src/tasks.py
@@ -105,16 +105,16 @@ def _process_ln_invoices_loop():
             gevent.sleep(5, False)
 
 def rebalance_channels(oscid: str, iscid: str, amount: int):
-    gevent.sleep(10, False) # HACK: wait for the webserver to start
+    gevent.sleep(10, False)  # HACK: wait for the webserver to start
     LnRpc().rebalance_channel(oscid, iscid, amount)
     send_email('Channel Rebalance Successful', 'Rebalanced {0} -> {1} with {2} sats'.format(oscid, iscid, amount))
 
 def send_email(title: str, msg: str, receiving_email: str | None = None):
-    gevent.sleep(10, False) # HACK: wait for the webserver to start
+    gevent.sleep(10, False)  # HACK: wait for the webserver to start
     email_utils.send_email(logger, title, msg, receiving_email)
 
 def pay_to_invoice(invoice_str: str):
-    gevent.sleep(10, False) # HACK: wait for the ln server to start
+    gevent.sleep(10, False)  # HACK: wait for the ln server to start
     LnRpc().pay(invoice_str)
 #
 # Init tasks
